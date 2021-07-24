61908499086b924179c3511d6801d3c7dec6dc52
Will <will.lai@mores.com.tw>
2021-07-24 15:19:32 +0800

    feat: Init Page

---
+++ lib/Screens/Login/LoginScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'components/background.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Background(),
+    );
+  }
+}
---
+++ lib/Screens/Login/components/background.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Background extends StatelessWidget {
+  const Background({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height,
+    );
+  }
+}
