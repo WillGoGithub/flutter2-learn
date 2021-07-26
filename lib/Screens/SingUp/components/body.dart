8edbf8b079b800a8827000adf5eab722e5b848e3
Will <will.lai@mores.com.tw>
2021-07-26 21:53:20 +0800

    fix: SingleChildScrollView

--- lib/Screens/SingUp/components/body.dart
+++ lib/Screens/SingUp/components/body.dart
@@ -16,58 +16,60 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'SignUp',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: size.height * 0.03,
-          ),
-          SvgPicture.asset(
-            'assets/icons/signup.svg',
-            height: size.height * 0.35,
-          ),
-          RoundedInputField(
-              hintText: 'Your Email',
-              icon: Icons.person,
-              onChanged: (value) {}),
-          RoundedPasswordField(onChanged: (value) {}),
-          RoundedButton(
-            text: 'SIGNUP',
-            pressed: () {},
-          ),
-          SizedBox(
-            height: size.height * 0.03,
-          ),
-          HaveAnAccountTip(
-            login: false,
-            tap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => LoginScreen()));
-            },
-          ),
-          TextDivider(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SocialIcon(
-                iconSrc: 'assets/icons/facebook.svg',
-                tap: () {},
-              ),
-              SocialIcon(
-                iconSrc: 'assets/icons/twitter.svg',
-                tap: () {},
-              ),
-              SocialIcon(
-                iconSrc: 'assets/icons/google-plus.svg',
-                tap: () {},
-              ),
-            ],
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'SignUp',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            SvgPicture.asset(
+              'assets/icons/signup.svg',
+              height: size.height * 0.35,
+            ),
+            RoundedInputField(
+                hintText: 'Your Email',
+                icon: Icons.person,
+                onChanged: (value) {}),
+            RoundedPasswordField(onChanged: (value) {}),
+            RoundedButton(
+              text: 'SIGNUP',
+              pressed: () {},
+            ),
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            HaveAnAccountTip(
+              login: false,
+              tap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()));
+              },
+            ),
+            TextDivider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SocialIcon(
+                  iconSrc: 'assets/icons/facebook.svg',
+                  tap: () {},
+                ),
+                SocialIcon(
+                  iconSrc: 'assets/icons/twitter.svg',
+                  tap: () {},
+                ),
+                SocialIcon(
+                  iconSrc: 'assets/icons/google-plus.svg',
+                  tap: () {},
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
